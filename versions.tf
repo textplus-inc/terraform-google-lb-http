751e7fcc195ff44b335f4fd401edc75651ebccb1
Joel Serrano <joel@textplus.com>
2020-02-12 12:26:31 -0800

    pin tf version

--- versions.tf
+++ versions.tf
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 terraform {
-  required_version = "~> 0.12.20"
+  required_version = ">= 0.12"
 }

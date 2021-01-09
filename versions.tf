7db215d0bf2aed3bb98ca327a6eaa2d80d39390a
Joel Serrano <joel@textplus.com>
2021-01-08 18:51:57 -0800

    terraform 0.13upgrade

--- versions.tf
+++ versions.tf
@@ -14,5 +14,13 @@
  * limitations under the License.
  */
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+    }
+  }
 }
